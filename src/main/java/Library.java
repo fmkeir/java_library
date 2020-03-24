df1fa9575ed50603552320e8a7412b35496e73b9
FKeir <fmkeir@gmail.com>
2020-03-24 19:04:47 +0000

    Use hashmap to count books by genre

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Library {
     private ArrayList<Book> books;
     private int capacity;
+    private HashMap<String, Integer> genreCount;
 
     public Library (int capacity) {
         this.books = new ArrayList<Book>();
         this.capacity = capacity;
+        this.genreCount = new HashMap<String, Integer>();
     }
 
     public int countBooks() {
@@ -20,15 +23,35 @@ public class Library {
     public void addBook(Book book) {
         if (canAddBook()) {
             this.books.add(book);
+            this.updateGenreCount(book);
         }
     }
 
     public Book removeBook(String bookname) {
         for (Book book : this.books) {
-            if (book.getName() == bookname) {
+            if (book.getName().equals(bookname)) {
                 return book;
             }
         }
         return null;
+    }
+
+    public void updateGenreCount(Book book) {
+        String genre = book.getGenre();
+        int count;
+        if (this.genreCount.containsKey(genre)) {
+            count = this.genreCount.get(genre) + 1;
+        } else {
+            count = 1;
+        }
+        this.genreCount.put(genre, count);
+    }
+
+    public int countBooksByGenre(String genre) {
+        if (this.genreCount.containsKey(genre)) {
+            return this.genreCount.get(genre);
+        } else {
+            return 0;
+        }
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -5,14 +5,18 @@ import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
     private Library library;
+    private Library bigLibrary;
     private Book book1;
     private Book book2;
+    private Book book3;
 
     @Before
     public void before(){
         library = new Library(1);
+        bigLibrary = new Library(5);
         book1 = new Book("Harry Potter", "JK Rowling", "Fantasy");
         book2 = new Book("Deep Work", "Cal Newport", "Non-fiction");
+        book3 = new Book("With Winning in Mind", "Lanny Bassham", "Non-fiction");
     }
 
     @Test
@@ -44,5 +48,13 @@ public class LibraryTest {
     public void cantRemoveBookIfNotThere() {
         Book removedBook = library.removeBook("Lord of the Rings");
         assertEquals(null, removedBook);
+    }
+
+    @Test
+    public void canGetNumberOfBooksByGenre() {
+        bigLibrary.addBook(book1);
+        bigLibrary.addBook(book2);
+        bigLibrary.addBook(book3);
+        assertEquals(2, bigLibrary.countBooksByGenre("Non-fiction"));
     }
 }
